b65c3d6cdc1d85b140f1c856f156ddf7c7c3d07b
Jeff <jefftully@gmail.com>
2019-08-26 13:39:21 -0400

    Working now  with version  control

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -6,13 +6,20 @@ router.route("/").get((req, res) => {
     .then(exercises => res.json(exercises))
     .catch(err => res.status(400).json("Error: " + err));
 });
+
 router.route("/add").post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
   const duration = Number(req.body.duration);
   const date = Date.parse(req.body.date);
 
-  const newExercise = new Exercise({ username, description, duration, date });
+  const newExercise = new Exercise({
+    username,
+    description,
+    duration,
+    date
+  });
+
   newExercise
     .save()
     .then(() => res.json("Exercise added!"))
@@ -24,11 +31,13 @@ router.route("/:id").get((req, res) => {
     .then(exercise => res.json(exercise))
     .catch(err => res.status(400).json("Error: " + err));
 });
+
 router.route("/:id").delete((req, res) => {
   Exercise.findByIdAndDelete(req.params.id)
     .then(() => res.json("Exercise deleted."))
     .catch(err => res.status(400).json("Error: " + err));
 });
+
 router.route("/update/:id").post((req, res) => {
   Exercise.findById(req.params.id)
     .then(exercise => {
@@ -36,6 +45,7 @@ router.route("/update/:id").post((req, res) => {
       exercise.description = req.body.description;
       exercise.duration = Number(req.body.duration);
       exercise.date = Date.parse(req.body.date);
+
       exercise
         .save()
         .then(() => res.json("Exercise updated!"))
@@ -43,4 +53,5 @@ router.route("/update/:id").post((req, res) => {
     })
     .catch(err => res.status(400).json("Error: " + err));
 });
+
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
-import UsersList from "./components/users-list.component";
-import EditUser from "./components/edit-user.component";
 
 function App() {
   return (
@@ -16,12 +14,10 @@ function App() {
       <div className="container">
         <Navbar />
         <br />
-        <Route path="exercise/" exact component={ExercisesList} />
-        <Route path="exercise/edit/:id" component={EditExercise} />
-        <Route path="exercise/create" component={CreateExercise} />
-        <Route path="user/create" component={CreateUser} />
-        <Route path="user/" exact component={UsersList} />
-        <Route path="user/edit/:id" component={EditUser} />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
       </div>
     </Router>
   );
